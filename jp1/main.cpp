28f069d601f8e2240444bafbb520e5cf37c585d0
Paweł Grad <UL0204422@edu.uni.lodz.pl>
2023-10-22 12:08:03 +0200

    1

--- jp1/main.cpp
+++ jp1/main.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 int main()
 {
-    int a = 0;
-    cin >> a;
-    cout << "Hello world!" << endl;
+    int age;
+    cout << "Ile masz lat?" << endl;
+    cin >> age;
+    cout << "Juz wiem! Masz " << age << " lat!" << endl;
+
+    age >= 18 ? cout << "Jestes pelnoletni!" : cout << "Nie jestes pelnoletni!";
+
+//    if (age >= 18) {
+//        cout << "Jestes pelnoletni!" << endl;
+//    } else {
+//        cout << "Nie jestes pelnoletni!" << endl;
+//    }
+
     return 0;
 }
